56a3b9cdf2436742f48e9a453dd54703906cd2fb
JarlTuxen <jarl.tuxen@gmail.com>
2020-03-19 10:25:35 +0100

    animals added to view model in index

--- src/main/java/dk/kea/spring_mvc/controller/HomeController.java
+++ src/main/java/dk/kea/spring_mvc/controller/HomeController.java
@@ -1,5 +1,7 @@
 package dk.kea.spring_mvc.controller;
 
+import dk.kea.spring_mvc.service.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController
 {
+    @Autowired
+    AnimalService animalService;
+
     @GetMapping("/")
     public String index(Model model)
     {
+        //add all animals to view model from animalService
+        model.addAttribute("anim", animalService.readAll());
         return("index");
     }
 
